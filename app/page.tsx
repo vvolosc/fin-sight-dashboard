0cf9467b7233a68b8b1fbcb1c8f94d9ce08e771f
Viktoria <voloshchuk.viktoria.92@gmail.com>
2026-02-18 13:42:00 +0100

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function App() {
   }
 
     
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   useEffect(() => {
     listTodos();
@@ -42,6 +42,7 @@ export default function App() {
 
   return (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
